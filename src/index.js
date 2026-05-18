5366f2c6cfb0afb76c2cd980634d6032bdaba270
VICKY2k02 <gvk.vicky02@gmail.com>
2026-05-18 16:15:09 +0530

    Components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import UserProfileCard from './UserProfiles/UserProfileCard';
-import UserProfileCards from './UserProfiles/UserProfileCards';
+//import UserProfileCard from './UserProfiles/UserProfileCard';
+import App from './components/UserCardMain';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UserProfileCard/>
+    {/*<UserProfileCard/>*/}
+    <App/>
   </React.StrictMode>
 );
 
